75dd4e1a3c5f799433bef6f10388f78f9c11afcf
crosszay1 <104384163+crosszay1@users.noreply.github.com>
2022-10-23 07:12:47 -0700

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/crosszay1/deadbox2/main/1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/crosszay1/deadbox2/main/main.lua"))()
