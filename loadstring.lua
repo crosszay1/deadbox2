327c26d2f2fa891653623a1125c2041de0ad7ea0
crosszay1 <104384163+crosszay1@users.noreply.github.com>
2022-10-31 12:38:45 -0700

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("mainscript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/crosszay1/deadbox2/main/redbox%202%20script%20main%20gui%20thing%20epic.lua"))()
